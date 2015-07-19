926dab41299c643ba35f6ac1fdf01425b0e60df1
Sandeep Sehdev <sksehdev@gmail.com>
2015-07-19 18:58:58 -0400

    adding modified python file for project 2

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,25 +13,49 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    DB = connect()
+    c = DB.cursor()
+    c.execute("delete from Matches;")
+    DB.commit()
+    DB.close()
+
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    DB = connect()
+    c = DB.cursor()
+    c.execute("delete from Players;")
+    DB.commit()
+    DB.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
 
+    DB = connect()
+    c = DB.cursor()
+    c.execute("select count(*) from Players;")
+    count = c.fetchone()[0]
+    DB.close()
+    return count
+
+
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    DB = connect()
+    c = DB.cursor()
+    c.execute("insert into Players (Name) values(%s);",(name,))
+    DB.commit()
+    DB.close()
+
 
 
 def playerStandings():
@@ -47,6 +71,13 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    DB = connect()
+    c = DB.cursor()
+    c.execute('select winner_id,winner_name,wins,wins+loses as matches from v_matches order by wins desc;')
+    players = c.fetchall()
+    #print "The array is " + str(player)
+    DB.close()
+    return players
 
 
 def reportMatch(winner, loser):
@@ -56,16 +87,22 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    DB = connect()
+    c = DB.cursor()
+    c.execute("insert into Matches (winner,loser) values(%s,%s);",(winner,loser,))
+    DB.commit()
+    DB.close()
+
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +110,16 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    player_standings = playerStandings()
+    swiss_pairings = []
+    for i in range(0,len(player_standings),2):
+      swiss_pairings.append([player_standings[i][0],player_standings[i][1],player_standings[i+1][0],player_standings[i+1][1]])
+
+    for j in range(0,len(swiss_pairings)):
+      swiss_pairings[j] = tuple(swiss_pairings[j])
+
+    return swiss_pairings
+
+
 
 
