f922f6af2991a9b4dd3c81cf41f9d9e7d9a62797
Sandeep Sehdev <sksehdev@gmail.com>
2015-07-21 22:59:31 -0400

    Modified SQL file to add change according to the review

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -17,19 +17,33 @@ CREATE TABLE Players(Player_ID serial,Name text,primary key(Player_ID));
 
 -- Create Matches table to store all the Matches between Players
 
-CREATE TABLE Matches(Match_ID serial , Winner int References Players(Player_ID), Loser int References Players(Player_ID), primary key(Match_ID));
+CREATE TABLE Matches(Match_ID serial , Winner int References Players(Player_ID),
+	                 Loser int References Players(Player_ID),
+	                 primary key(Match_ID));
 
 
 
 --View to see all players and their number of wins
-CREATE VIEW v_winners as select player_id as winner_id,name as winner_name,count(matches.winner) as wins from Players left join Matches on Players.player_id = Matches.winner group by player_id;
+CREATE VIEW v_winners as select player_id as winner_id,name as
+                         winner_name,count(matches.winner) as wins from
+                         Players left join Matches on Players.player_id = Matches.winner
+                         group by player_id;
 
 --View to see all players and their number of losses
-CREATE VIEW v_losers as select player_id as loser_id,name as loser_name,count(matches.loser) as loses from Players left join Matches on Players.player_id = Matches.loser group by player_id;
---create view v_matches as select winner_id,winner_name,wins,loses from v_winners join v_losers on v_winners.winner_id = v_losers.loser_id;
+CREATE VIEW v_losers as select player_id as loser_id,name as
+                        loser_name,count(matches.loser) as loses from Players left join Matches on
+                        Players.player_id = Matches.loser group by player_id;
+
+
 
 --View to see all players (ID and name) and their wins and losses.
-CREATE VIEW v_matches as select winner_id,winner_name,wins,loses from v_winners join v_losers on v_winners.winner_id = v_losers.loser_id;
+CREATE VIEW v_matches as select winner_id,winner_name,wins,loses from
+                         v_winners join v_losers on
+                         v_winners.winner_id = v_losers.loser_id;
 
+--View to see all players standings
+
+CREATE VIEW v_players as select winner_id,winner_name,wins,wins+loses as
+                         matches from v_matches order by wins desc;
 
 
